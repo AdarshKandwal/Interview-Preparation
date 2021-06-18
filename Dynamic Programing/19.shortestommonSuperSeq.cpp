138bbbdf6e6f3b9da818e13abf37600162fb28f0
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2021-06-18 08:31:17 +0530

    Create 19.shortestommonSuperSeq.cpp

---
+++ Dynamic Programing/19.shortestommonSuperSeq.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(char *a , char *b , int x ,int y,vector<vector<int>> &t){
+    
+    for(int i=0;i<x+1;i++){
+    for(int j=0;j<y+1;j++){
+     if(i==0 || j==0)
+        t[i][j]=0;
+    else if(a[i-1] == b[j-1])
+        t[i][j]=1+t[i-1][j-1];     
+    else
+        t[i][j]=max(t[i-1][j],t[i][j-1]);
+    }}
+    
+ return t[x][y];
+}
+
+
+int main()
+{
+    char X[] = "AGGTAB"; 
+    char Y[] = "GXTXAYB"; 
+      
+    int m = strlen(X); 
+    int n = strlen(Y); 
+    vector<vector<int>> t(m+1,vector<int>(n+1));
+    int lcsvar=lcs(X,Y,m,n,t);
+    int lensuperseq=m+n-lcsvar;
+    cout<<"length of Super Sequence : "<<lensuperseq;
+    
+    return 0;
+    
+}
