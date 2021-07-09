8bf8ffb61c0bcebc6aef32a385ccf64cc4e64a84
Adarsh kandwal <41911887+AdarshKandwal@users.noreply.github.com>
2021-07-09 18:01:54 +0530

    Create 7.CheckIfStringsAreRotation.cpp

---
+++ Strings/7.CheckIfStringsAreRotation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool areRotation(string &s1,string &s2){
+    if(s1.length() != s2.length())
+        return false;
+    return ((s1+s2).find(s2) != string::npos);
+}
+
+int main(){
+    string s1="ABCD";
+    string s2="CDAB";
+    cout<<areRotation(s1,s2);
+    return 0;
+}
